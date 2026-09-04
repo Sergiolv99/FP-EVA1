0aa9f7262c03ccfa49112b3168dd74aac8645c04
Sergiolv99 <L26550216@chihuahua2.TECNM.MX>
2026-09-03 18:38:21 -0600

    Add files via upload

---
+++ EVA1_2_HOLA_MUNDO/src/eva1_2_hola_mundo/EVA1_2_HOLA_MUNDO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_hola_mundo;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_2_HOLA_MUNDO {
+    public static void main(String[] args) {// PROGRAM START
+       // THIS IS THE CODE BLOCK
+       // MOSTRAR MENSAJEJE DE TEXTO
+       // MOSTRAR--INSTRUCCION
+       // MOSTRAR--DATOS
+       
+       System.out.print("HOLA MUNDO!!");
+    }// END OF LANGUAGE
+    
+}

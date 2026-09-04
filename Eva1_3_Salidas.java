560d573ca093ed368d8a8054438218e128fc734f
Sergiolv99 <L26550216@chihuahua2.TECNM.MX>
2026-09-03 18:45:04 -0600

    Add files via upload

---
+++ Eva1_3_Salidas.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Fundamentos de Programacion");
+        System.out.println("Sergio Lucero Valdiviezo");
+        System.out.print("Tu calificacion sera:");
+        System.out.println(85);
+    }
+}
